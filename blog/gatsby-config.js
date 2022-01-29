11032fc7077c3e8c3f466d17f8a310f77ee50211
Katie Johnston <KatieJRise1995@gmail.com>
2022-01-29 12:45:56 -0600

    Completed Activity 1
    Completed Activity 1

---
+++ blog/gatsby-config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  siteMetadata: {
+    title: `Hi! I'm Katie! Welcome to my Gatsby Project`,
+    description: `This project is for ITDEV-164, Web Programming 2`,
+    author: `@KatieJRise1995`,
+    contact: {
+      name: `Katie S. Johnston`,
+      company: `Northwestern Mutual/Milwaukee Area Technical College/University of Wisconsin - Milwaukee`,
+      address: `123 Fake Street`
+    },
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
